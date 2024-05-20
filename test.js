2a3fe147209fd16ee6ea83bfa2f6c3bcab795973
vivek <viv2005ek@gmail.com>
2024-05-20 18:12:27 +0530

    ok

---
+++ test.js
@@ -0,0 +1,6 @@
+const calculator = require("./calc"); // Corrected path
+
+console.log(calculator.addition(3, 4));
+console.log(calculator.subtraction(3, 4));
+console.log(calculator.multiply(3, 4));
+console.log(calculator.divide(3, 4));
