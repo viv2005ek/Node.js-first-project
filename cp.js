08eb49d44da43e2f6f64f1e3485f37ccd7adddf7
vivek <viv2005ek@gmail.com>
2024-05-20 20:23:28 +0530

    push

---
+++ cp.js
@@ -0,0 +1,19 @@
+let cp = require("child_process"); //child process module
+
+cp.execSync("calc"); //open calculator    
+//more functions-->
+//calc=> open calculator
+//  notepad=>Opens the Notepad application,
+//  mspaint=>Opens Microsoft Paint,
+//  taskmgr=>Opens the Task Manager,
+//  control=>Opens the Control Panel,
+//  cmd=>Opens a new Command Prompt window,
+//  explorer=>Opens File Explorer,
+//  regedit=>Opens the Registry Editor.
+
+
+cp.execSync("start chrome"); //open chrome
+
+cp.execSync("start chrome https://github.com/viv2005ek"); //open this link on chrome
+
+console.log("" + cp.execSync("node test.js")); //open this js file   "" nesseary to convert node test.js output in string
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const calculator = require("./calc"); // Corrected path
 
+console.log("calculations");
 console.log(calculator.addition(3, 4));
 console.log(calculator.subtraction(3, 4));
 console.log(calculator.multiply(3, 4));
